2f97c14f261593b266641a858ebfab89e8498dd3
ksarapultsev <ksarapultsev2014@gmail.com>
2018-09-05 11:15:00 +0600

    classwork2. add colors

---
+++ app/src/main/java/com/kos/work/moneytracker/ItemListActivity.java
@@ -0,0 +1,13 @@
+package com.kos.work.moneytracker;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class ItemListActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_items_list);
+    }
+}
